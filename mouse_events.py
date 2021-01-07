8f59b7028ba55f760158ceb2df23effb6a067be4
BuildTools <unconfigured@null.spigotmc.org>
2021-01-07 14:03:21 -0500

    V1.4 Ball drop working on all stages 1-5

--- mouse_events.py
+++ mouse_events.py
@@ -33,29 +33,29 @@ class MouseEvents:
                     else:
                         print('Invalid Ball Location')
                 elif stage.gameStage == 2:
-                    if mousePosition[0] in range(100, 200) and mousePosition[1] in range(100, 200):
+                    if mousePosition[0] in range(380, 510) and mousePosition[1] in range(90, 150):
                         print("in box")
                         state.ballState = 'placed'
                     else:
-                        pass
+                        print('Invalid Ball Location')
                 elif stage.gameStage == 3:
-                    if mousePosition[0] in range(100, 200) and mousePosition[1] in range(100, 200):
+                    if mousePosition[0] in range(494, 530) and mousePosition[1] in range(66, 153):
                         print("in box")
                         state.ballState = 'placed'
                     else:
-                        pass
+                        print('Invalid Ball Location')
                 elif stage.gameStage == 4:
-                    if mousePosition[0] in range(100, 200) and mousePosition[1] in range(100, 200):
+                    if mousePosition[0] in range(210, 348) and mousePosition[1] in range(493, 539):
                         print("in box")
                         state.ballState = 'placed'
                     else:
-                        pass
+                        print('Invalid Ball Location')
                 elif stage.gameStage == 5:
-                    if mousePosition[0] in range(100, 200) and mousePosition[1] in range(100, 200):
+                    if mousePosition[0] in range(512, 545) and mousePosition[1] in range(45, 127):
                         print("in box")
                         state.ballState = 'placed'
                     else:
-                        pass
+                        print('Invalid Ball Location')
                 else:
                     pass
 

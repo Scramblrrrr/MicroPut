d7dd1b8258786b87ad54f1e476970c0dffbd7804
BuildTools <unconfigured@null.spigotmc.org>
2021-01-13 20:18:29 -0500

    Added a slider from a stack overflow project, originally used for gradient colour waves. In the process of repurposing it to be used to select the angle at which the ball will be hit at. Appears during the speed selection process

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import time
 import mouse_events
 from lib.sprites import *
 import math
-
+import pygame, math, sys
 
 pygame.init()   # starts the pygame environment
 # instantiate screen object
@@ -45,7 +45,7 @@ class Ball(pygame.sprite.Sprite):
         self.image = sprites['Golf Ball']
         self.rect = self.image.get_rect()
         self.rect.center = (165, 500)
-        self.angle = math.radians(180+95)
+        self.angle = math.radians(270)
         self.speed = 7
         self.dx = math.cos(self.angle) * self.speed
         self.dy = math.sin(self.angle) * self.speed
@@ -73,6 +73,81 @@ class Ball(pygame.sprite.Sprite):
         self.dx = math.cos(self.angle) * self.speed
         self.dy = math.sin(self.angle) * self.speed
 
+# SLIDERS
+
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 50, 50)
+YELLOW = (255, 255, 0)
+GREEN = (0, 255, 50)
+BLUE = (50, 50, 255)
+GREY = (200, 200, 200)
+ORANGE = (200, 100, 50)
+CYAN = (0, 255, 255)
+MAGENTA = (255, 0, 255)
+TRANS = (1, 1, 1)
+
+font = pygame.font.SysFont("Verdana", 12)
+
+class Slider():
+    def __init__(self, name, val, maxi, mini, pos):
+        self.val = val  # start value
+        self.maxi = maxi  # maximum at slider position right
+        self.mini = mini  # minimum at slider position left
+        self.xpos = pos  # x-location on screen
+        self.ypos = 590
+        self.surf = pygame.surface.Surface((100, 50))
+        self.hit = False  # the hit attribute indicates slider movement due to mouse interaction
+
+        self.txt_surf = font.render(name, 1, BLACK)
+        self.txt_rect = self.txt_surf.get_rect(center=(50, 15))
+
+        # Static graphics - slider background #
+        self.surf.fill((100, 100, 100))
+        pygame.draw.rect(self.surf, GREY, [0, 0, 100, 50], 3)
+        pygame.draw.rect(self.surf, ORANGE, [10, 10, 80, 10], 0)
+        pygame.draw.rect(self.surf, WHITE, [10, 30, 80, 5], 0)
+
+        self.surf.blit(self.txt_surf, self.txt_rect)  # this surface never changes
+
+        # dynamic graphics - button surface #
+        self.button_surf = pygame.surface.Surface((20, 20))
+        self.button_surf.fill(TRANS)
+        self.button_surf.set_colorkey(TRANS)
+        pygame.draw.circle(self.button_surf, BLACK, (10, 10), 6, 0)
+        pygame.draw.circle(self.button_surf, ORANGE, (10, 10), 4, 0)
+
+    def draw(self):
+        """ Combination of static and dynamic graphics in a copy of
+    the basic slide surface
+    """
+        # static
+        surf = self.surf.copy()
+
+        # dynamic
+        pos = (10+int((self.val-self.mini)/(self.maxi-self.mini)*80), 33)
+        self.button_rect = self.button_surf.get_rect(center=pos)
+        surf.blit(self.button_surf, self.button_rect)
+        self.button_rect.move_ip(self.xpos, self.ypos)  # move of button box to correct screen position
+
+        # screen
+        screen.blit(surf, (self.xpos, self.ypos))
+
+    def move(self):
+        """
+    The dynamic part; reacts to movement of the slider button.
+    """
+        self.val = (pygame.mouse.get_pos()[0] - self.xpos - 10) / 80 * (self.maxi - self.mini) + self.mini
+        if self.val < self.mini:
+            self.val = self.mini
+        if self.val > self.maxi:
+            self.val = self.maxi
+
+angle = Slider("Angle", 270, 90, 450, 455)
+
+slides = [angle]
+
+# END OF SLIDERS
 
 class Bumper(pygame.sprite.Sprite):
 
@@ -188,6 +263,10 @@ def play():
                 gameBall.rect.center = (mx+8, my+8)
                 ball_group.draw(screen)
             while game.ballState == 'selecting speed':
+                for s in slides:
+                    if s.hit:
+                        s.move()
+                    s.draw()
                 time.sleep(1/30)
                 renderMap(mapFile=SpeedChart, row=575, column=100)
                 if gameBall.speed >= 1 and gameBall.speed <= 6:
@@ -212,6 +291,7 @@ def play():
                 bumper_hits = pygame.sprite.spritecollide(gameBall,bumper_group, False, pygame.sprite.collide_mask)
                 if bumper_hits:
                     gameBall.bumperhit(bumper1)
+            #     if ballsprite center is over black (hole), ball disappears, score
             if gameBall.speed == 0:
                 print("Stopped!")
                 # create a sequence that allows the player to change direction and speed again and again until they get the ball in the whole while keeping score (amount of "swings")
